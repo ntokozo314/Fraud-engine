28b95b7c0a4d233ab6953fd210008b12fdebf899
ntokozomngadi <mngadi.n07@gmail.com>
2025-11-21 18:20:51 +0200

    Fixed compose file and added audit tables

--- src/main/resources/db/migration/V1__Inital_fraud_tables.sql
+++ src/main/resources/db/migration/V1__Inital_fraud_tables.sql
@@ -3,3 +3,26 @@ CREATE TABLE bad_beneficiary (
     branch_code VARCHAR(16) NOT NULL,
     account_number VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE transaction (
+    transaction_id UUID PRIMARY KEY,
+    branch_code VARCHAR(16) NOT NULL,
+    account_number VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    suspected_fraud VARCHAR(32)
+);
+
+CREATE TABLE transaction_evaluation(
+    transaction_id UUID,
+    evaluation_id UUID,
+    PRIMARY KEY (transaction_id, evaluation_id)
+);
+
+
+CREATE TABLE evaluation(
+    evaluation_id UUID PRIMARY KEY,
+    evaluator_name VARCHAR(64),
+    reason VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    metadata jsonb
+);
